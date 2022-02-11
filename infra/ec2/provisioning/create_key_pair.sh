ff188a41b91913bf7742ead52e015f6ab6f0e872
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-11 20:29:08 +0300

    hardcode bucket name

--- infra/ec2/provisioning/create_key_pair.sh
+++ infra/ec2/provisioning/create_key_pair.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-object_exists=$(aws s3api head-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem || true)
+# object_exists=$(aws s3api head-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem || true)
+# if [ -z "$object_exists" ]; then
+#     echo "it does not exist, creating a fresh key pair and uploading"
+#     ssh-keygen -t rsa -b 2048 -f ~/.ssh/ubuntu_ec2.pem -q -P ''
+#     chmod 400 ~/.ssh/ubuntu_ec2.pem
+#     ssh-keygen -y -f ~/.ssh/ubuntu_ec2.pem > ~/.ssh/ubuntu_ec2.pub
+#     aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem --body ~/.ssh/ubuntu_ec2.pem
+#     aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub --body ~/.ssh/ubuntu_ec2.pub
+# else
+#     echo "it exists, downloading existing"
+#     echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem ~/.ssh/ubuntu_ec2.pem
+#     echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub ~/.ssh/ubuntu_ec2.pub
+# fi
+
+# object_exists=$(aws s3api head-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem || true)
+object_exists=$(aws s3api head-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem || true)
 if [ -z "$object_exists" ]; then
     echo "it does not exist, creating a fresh key pair and uploading"
     ssh-keygen -t rsa -b 2048 -f ~/.ssh/ubuntu_ec2.pem -q -P ''
-    chmod 400 ~/.ssh/ubuntu_ec2.pem
     ssh-keygen -y -f ~/.ssh/ubuntu_ec2.pem > ~/.ssh/ubuntu_ec2.pub
-    aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem --body ~/.ssh/ubuntu_ec2.pem
-    aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub --body ~/.ssh/ubuntu_ec2.pub
+    # aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem --body ~/.ssh/ubuntu_ec2.pem
+    aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem --body ~/.ssh/ubuntu_ec2.pem
+    # aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub --body ~/.ssh/ubuntu_ec2.pub
+    aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub --body ~/.ssh/ubuntu_ec2.pub
 else
     echo "it exists, downloading existing"
-    echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem ~/.ssh/ubuntu_ec2.pem
-    echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub ~/.ssh/ubuntu_ec2.pub
+    # echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem ~/.ssh/ubuntu_ec2.pem
+    echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem ~/.ssh/ubuntu_ec2.pem
+    # echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub ~/.ssh/ubuntu_ec2.pub
+    echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub ~/.ssh/ubuntu_ec2.pub
 fi
+
+chmod 600 ~/.ssh/ubuntu_ec2.pem
+chmod 600 ~/.ssh/ubuntu_ec2.pub
