dde4caa7135d4684c370f947e4d6873668de3729
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-11 23:44:50 +0300

    change s3 state prefix

--- infra/ec2/terraform.tf
+++ infra/ec2/terraform.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "BUCKET_NAME"
-    key     = "state/BUCKET_KEY_PATH"
+    key     = "states/BUCKET_KEY_PATH"
     region  = "AWS_REGION"
     encrypt = true
   }
