b9441aa3e5b68c992a3e765d710f64e0d22ceb5b
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-11 21:44:40 +0300

    revert script

--- infra/ec2/provisioning/create_key_pair.sh
+++ infra/ec2/provisioning/create_key_pair.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+ssh-keygen -t rsa -b 2048 -f /tmp/ubuntu_ec2.pem -q -P ''
+chmod 400 /tmp/ubuntu_ec2.pem
+ssh-keygen -y -f /tmp/ubuntu_ec2.pem > /tmp/ubuntu_ec2.pub
+
 # object_exists=$(aws s3api head-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem || true)
 # if [ -z "$object_exists" ]; then
-aws s3 ls s3://skysports-my-tf-states/keys/ubuntu_ec2.pem
-if [[ $? -ne 0 ]]; then
-    echo "it does not exist, creating a fresh key pair and uploading"
-    ssh-keygen -t rsa -b 2048 -f /tmp/ubuntu_ec2.pem -q -P ''
-    ssh-keygen -y -f /tmp/ubuntu_ec2.pem > /tmp/ubuntu_ec2.pub
-    aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem --body /tmp/ubuntu_ec2.pem
-    aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub --body /tmp/ubuntu_ec2.pub
-else
-    echo "it exists, downloading existing"
-    echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem /tmp/ubuntu_ec2.pem
-    echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub /tmp/ubuntu_ec2.pub
-fi
+# aws s3 ls s3://skysports-my-tf-states/keys/ubuntu_ec2.pem
+# if [[ $? -ne 0 ]]; then
+#     echo "it does not exist, creating a fresh key pair and uploading"
+#     ssh-keygen -t rsa -b 2048 -f /tmp/ubuntu_ec2.pem -q -P ''
+#     ssh-keygen -y -f /tmp/ubuntu_ec2.pem > /tmp/ubuntu_ec2.pub
+#     aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem --body /tmp/ubuntu_ec2.pem
+#     aws s3api put-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub --body /tmp/ubuntu_ec2.pub
+# else
+#     echo "it exists, downloading existing"
+#     echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pem /tmp/ubuntu_ec2.pem
+#     echo "y" | aws s3api get-object --bucket skysports-my-tf-states --key keys/ubuntu_ec2.pub /tmp/ubuntu_ec2.pub
+# fi
 
-chmod 400 /tmp/ubuntu_ec2.pem
-chmod 400 /tmp/ubuntu_ec2.pub
+# chmod 400 /tmp/ubuntu_ec2.pem
+# chmod 400 /tmp/ubuntu_ec2.pub
