798bf8f7feb71263ad1a06c22019b2f0a1f3be09
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-11 20:24:26 +0300

    change file permission

--- infra/ec2/provisioning/create_key_pair.sh
+++ infra/ec2/provisioning/create_key_pair.sh
@@ -4,6 +4,7 @@ object_exists=$(aws s3api head-object --bucket <BUCKET_NAME> --key keys/ubuntu_e
 if [ -z "$object_exists" ]; then
     echo "it does not exist, creating a fresh key pair and uploading"
     ssh-keygen -t rsa -b 2048 -f ~/.ssh/ubuntu_ec2.pem -q -P ''
+    chmod 400 ~/.ssh/ubuntu_ec2.pem
     ssh-keygen -y -f ~/.ssh/ubuntu_ec2.pem > ~/.ssh/ubuntu_ec2.pub
     aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem --body ~/.ssh/ubuntu_ec2.pem
     aws s3api put-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub --body ~/.ssh/ubuntu_ec2.pub
@@ -12,6 +13,3 @@ else
     echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pem ~/.ssh/ubuntu_ec2.pem
     echo "y" | aws s3api get-object --bucket <BUCKET_NAME> --key keys/ubuntu_ec2.pub ~/.ssh/ubuntu_ec2.pub
 fi
-
-chmod 600 ~/.ssh/ubuntu_ec2.pem
-chmod 600 ~/.ssh/ubuntu_ec2.pub
