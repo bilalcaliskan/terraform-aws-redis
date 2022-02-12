2e0b1c2f3f569107e919d338b6a33c4f631d8ed3
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-12 16:33:17 +0300

    proper image names

--- infra/ec2/provisioning/create_key_pair.sh
+++ infra/ec2/provisioning/create_key_pair.sh
@@ -2,13 +2,13 @@
 
 object_exists=$(aws s3api head-object --bucket BUCKET_NAME --key keys/ubuntu_ec2.pem || true)
 if [ -z "$object_exists" ]; then
-    echo "it does not exist, creating a fresh key pair and uploading"
+    echo "it does not exist, creating a fresh key pair and uploading to S3"
     ssh-keygen -t rsa -b 2048 -f /home/runner/ubuntu_ec2.pem -q -P ''
     ssh-keygen -y -f /home/runner/ubuntu_ec2.pem > /home/runner/ubuntu_ec2.pub
     aws s3api put-object --bucket BUCKET_NAME --key keys/ubuntu_ec2.pem --body /home/runner/ubuntu_ec2.pem
     aws s3api put-object --bucket BUCKET_NAME --key keys/ubuntu_ec2.pub --body /home/runner/ubuntu_ec2.pub
 else
-    echo "it exists, downloading existing"
+    echo "it exists, downloading existing key pair from S3"
     echo "y" | aws s3api get-object --bucket BUCKET_NAME --key keys/ubuntu_ec2.pem /home/runner/ubuntu_ec2.pem
     echo "y" | aws s3api get-object --bucket BUCKET_NAME --key keys/ubuntu_ec2.pub /home/runner/ubuntu_ec2.pub
 fi
