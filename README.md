d9d53dd689bd479a366f0686b0a92f38a8408a50
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-02-18 16:31:28 +0300

    restructure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # terraform-aws-redis
+
+### References
+- https://blog.testdouble.com/posts/2021-12-07-elevate-your-terraform-workflow-with-github-actions/
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # version = ">= 3.0.0"
+      version = ">= 3.0.0"
     }
   }
 
@@ -13,6 +13,5 @@ terraform {
     encrypt = true
   }
 
-  # TODO: proper versioning
   required_version = "~> 1.1.0"
 }
