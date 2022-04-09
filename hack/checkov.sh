ebdc51cc0c636f9a43384f8dbe2ed6ee4b4724b5
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-04-09 16:44:30 +0300

    test pipeline failure

--- hack/checkov.sh
+++ hack/checkov.sh
@@ -3,4 +3,5 @@
 terraform init -input=false
 terraform plan -input=false -out tf.plan
 terraform show -json tf.plan > tf.json
-checkov -f tf.json --soft-fail-on CKV_AWS_126,CKV_AWS_24 --framework terraform_plan
+#checkov -f tf.json --soft-fail-on CKV_AWS_126,CKV_AWS_24 --framework terraform_plan
+checkov -f tf.json --soft-fail-on CKV_AWS_126 --framework terraform_plan
