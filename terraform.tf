738c1173f3833f499acedc4ea58cee78b38bd001
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-04-17 17:39:26 +0300

    chore: debug tf.json output

--- terraform.tf
+++ terraform.tf
@@ -6,19 +6,19 @@ terraform {
     }
   }
 
+/*  backend "s3" {
+    bucket  = "skysports-my-tf-states222"
+    key     = "states/redis.tf"
+    region  = "us-east-2"
+    encrypt = true
+  }*/
+
   backend "s3" {
     bucket  = "BUCKET_NAME"
     key     = "states/BUCKET_KEY_PATH"
     region  = "AWS_REGION"
     encrypt = true
   }
-
-  /*backend "s3" {
-    bucket  = "skysports-my-tf-states222"
-    key     = "states/redis.tf"
-    region  = "us-east-2"
-    encrypt = true
-  }*/
 
   required_version = "~> 1.1.0"
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "instance_count" {
-  description = "Number of EC2 instances to deploy"
-  type        = number
-  default     = 1
-}
-
-/*variable "instance_type" {
-  description = "Type of EC2 instance to use"
-  type        = string
-  default     = "t2.micro"
-}*/
-
-variable "instance_type" {
-  description = "Type of EC2 instance to use"
-  type        = string
-  default     = "AWS_INSTANCE_TYPE"
-}
-
 /*variable "aws_region" {
   description = "Region for resources"
   type        = string
@@ -28,16 +10,16 @@ variable "aws_region" {
   default     = "AWS_REGION"
 }
 
-variable "root_block_device_size" {
-  description = "Size of the root block device"
+/*variable "instance_type" {
+  description = "Type of EC2 instance to use"
   type        = string
-  default     = "10"
-}
+  default     = "t2.micro"
+}*/
 
-variable "instance_name_prefix" {
-  description = "Instance name prefix while creating EC2 instances"
+variable "instance_type" {
+  description = "Type of EC2 instance to use"
   type        = string
-  default     = "redisnode0"
+  default     = "AWS_INSTANCE_TYPE"
 }
 
 /*variable "public_key" {
@@ -54,6 +36,26 @@ variable "public_key" {
 
 variable "private_key" {
   default = "/home/runner/redis_ec2.pem"
+}
+
+// dsf
+
+variable "instance_count" {
+  description = "Number of EC2 instances to deploy"
+  type        = number
+  default     = 1
+}
+
+variable "root_block_device_size" {
+  description = "Size of the root block device"
+  type        = string
+  default     = "10"
+}
+
+variable "instance_name_prefix" {
+  description = "Instance name prefix while creating EC2 instances"
+  type        = string
+  default     = "redisnode0"
 }
 
 variable "ansible_user" {
