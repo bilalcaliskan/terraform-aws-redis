334fcb31cf86bf0ee177df3b04372c001d0af881
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-04-20 19:41:37 +0300

    chore: add dummy custom validation rules

--- variables.tf
+++ variables.tf
@@ -40,16 +40,32 @@ variable "private_key" {
 
 // dsf
 
+/*variable "instance_count" {
+  description = "Number of EC2 instances to deploy"
+  type        = number
+  default     = 1
+}*/
+
 variable "instance_count" {
   description = "Number of EC2 instances to deploy"
   type        = number
   default     = 1
+
+  validation {
+    condition     = var.instance_count > 0
+    error_message = "The instance_count value must be bigger than 0"
+  }
 }
 
 variable "root_block_device_size" {
   description = "Size of the root block device"
   type        = string
   default     = "10"
+
+  validation {
+    condition     = length(var.root_block_device_size) >= 10
+    error_message = "The root_block_device_size value must be at least 10"
+  }
 }
 
 variable "instance_name_prefix" {
